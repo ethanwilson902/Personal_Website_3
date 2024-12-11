a31bb88fa2df16702d7b31fc43346f5ad9938e58
ethanwilson902 <ethanwilson902@gmail.com>
2024-12-11 08:55:01 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue';
-
 export default {
-  components: {
-    Navbar,
-  },
+  name: 'App',
 };
 </script>
 
 <style>
-/* Global styles (if necessary) */
+/* Global styles, if any, can be added here */
 body {
   font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
 }
 </style>
